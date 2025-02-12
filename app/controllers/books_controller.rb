299574533c0af2ee16eff3390fabd3cacb2164ea
ndegz <ndegwa8781@gmail.com>
2025-02-12 16:53:56 +0300

    updated books and users controllers to handle borrowing and returning of books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,20 @@
 class BooksController < ApplicationController
+
+  before_action :authenticate_user!, only: [:borrow]
+
   def index
     @books = Book.all # get all books from the database
+  end
+
+  def borrow
+    @book = Book.find(params[:id])
+    borrowing  = current_user.borrowings.new(book: @book)
+
+    if borrowing.save
+      redirect_to user_borrowed_books_path(current_user), notice: 'Book borrowed successfully'
+    
+    else
+      redirect_to book_path, alert: borrowing.errors.full_messages.join(', ')
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+  before_action: authenticate_user!, only: [:borrowed_books, :return_book]
   # show the registration form
   def new
     @user = User.new
@@ -17,5 +19,18 @@ class UsersController < ApplicationController
   # strong parameters for user registration
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def borrowed_books
+    @borrowed_books = current_user.borrowed_books.where('borrowings.due_date > ?', Time.current)
+  end
+
+  def return_book
+    borrowing = current_user.borrowings.find(book_id: params[:id])
+    if borrowing&.destroy
+      redirect_to borrowed_books_user_path(current_user), notice: 'Book returned successfully'
+    else
+      redirect_to borrowed_books_user_path(current_user), alert: 'Book not found'
+    end
   end
 end
